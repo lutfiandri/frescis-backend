d2f6b9bac1d7288e8f9dde227cd0ac4345f392ec
lutfiandri <lutfiandri232@gmail.com>
2023-05-26 20:16:46 +0700

    feat(prediction): save image and result

--- Predict/__init__.py
+++ Predict/__init__.py
@@ -3,11 +3,23 @@ import tempfile
 import json
 
 import azure.functions as func
-from roboflow import Roboflow
 
+from roboflow import Roboflow
+from pymongo import MongoClient
+import cloudinary
+import cloudinary.uploader
+
+# Roboflow
 rf = Roboflow(api_key="HOCRxNRROICgFZfge4M8")
 project = rf.workspace().project("frescis")
 model = project.version(1).model
+
+# Cloudinary
+cloudinary.config(
+    cloud_name='drziipos7',
+    api_key='275238259143145',
+    api_secret='90LCTp-FwqSbpikx77HGItV8qt8'
+)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -29,20 +41,31 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # Make predictions on the image
     result = model.predict(tmp_file_path).json()
+
+    # Save image
+    response = cloudinary.uploader.upload(tmp_file_path)
+    logging.info("response", response)
+
+    result["image_url"] = str(response["secure_url"])
+    result["uid"] = req.form.get("uid")
     result_json = json.dumps(result)
+
+    # Save to histories
+    cosmos_uri = "mongodb://frescis-mongo:w6zrCWZbMjBWkPOLTD7BarZHD91ZRgJFh2j7KupPwIp2ciSlvSnNC2oUBjhk1Ju4jcFssNyCWJO2ACDbCB9q7w==@frescis-mongo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@frescis-mongo@"
+    client = MongoClient(cosmos_uri)
+
+    db = client["frescis-db"]
+    collection = db["histories"]
+
+    # result['uid'] = req.form.get('uid')
+    collection.insert_one(result)
+
+    client.close()
 
     headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST',
-        'Access-Control-Allow-Headers': 'Content-Type',
         "Content-Type": "application/json"
     }
-
-    # headers = {
-    #     'Access-Control-Allow-Origin': '*',
-    #     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    #     'Access-Control-Allow-Headers': 'Content-Type',
-    #     'Access-Control-Max-Age': '86400'  # Optional: Specify the maximum age of preflight requests
-    # }
 
     return func.HttpResponse(result_json, headers=headers)
